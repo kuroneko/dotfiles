3c8ad9c26a125d45f3bc09078aabcc71cbef0447
Chris Collins <kuroneko@sysadninjas.net>
2014-03-04 09:53:20 +1100

    Fix pyenv profile to work with locally installed, not just brew

--- .profile.d/80pyenv.zsh
+++ .profile.d/80pyenv.zsh
@@ -1,3 +1,12 @@
+if [ -z "${PYENV_ROOT}" ]; then
+	if [ -d "${HOME}/.pyenv" ]; then
+		PYENV_ROOT="${HOME}/.pyenv"
+		export PYENV_ROOT
+	fi
+fi
+if [ -n "${PYENV_ROOT}" ]; then
+	PATH="${PYENV_ROOT}/bin":"${PATH}"
+fi
 if which pyenv > /dev/null; then
   eval "$(pyenv init -)"
 fi
